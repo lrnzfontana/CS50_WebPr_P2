37c17677e40b62574c7a1620fb7bc64af6606e4d
Lorenzo <lrnz.fontana@gmail.com>
2018-11-24 15:23:18 +0000

    Add Heroku files

--- static/channelScript.js
+++ static/channelScript.js
@@ -95,7 +95,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-  var socket = io.connect("/");
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + 443);
+
 
   // When connected, configure button
   socket.on('connect', () => {
